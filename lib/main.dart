d7d641c97621126500439c14bfc222943472c533
nikhilchowdary-yamani <nikhilchowdary_yamani@srmap.edu.in>
2025-02-05 15:52:33 -0500

    Initial project setup and structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,22 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // This is the stateless widget
-      home: DefaultTabController(
-        length: 3,
-        child: _TabsNonScrollableDemo(),
-      ),
-    );
-  }
-}
-
-// THis is the stateful widget that stores the look of the tab
-class _TabsNonScrollableDemo extends StatefulWidget {
-  @override
-  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
-}
-
-class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
-    with SingleTickerProviderStateMixin, RestorationMixin {
-  late TabController _tabController;
-
-  final RestorableInt tabIndex = RestorableInt(0);
-
-  @override
-  String get restorationId => 'tab_non_scrollable_demo';
-
-  @override
-  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(tabIndex, 'tab_index');
-    _tabController.index = tabIndex.value;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      initialIndex: 0,
-      length: 4,
-      vsync: this,
-    );
-    _tabController.addListener(() {
-      setState(() {
-        tabIndex.value = _tabController.index;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    tabIndex.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-// For the ToDo task hint: consider defining the widget and name of the tabs here
-    final tabs = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
-
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(
-          'Activity 1',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Weather App'),
         ),
-        bottom: TabBar(
-          controller: _tabController,
-          isScrollable: false,
-          tabs: [
-            for (final tab in tabs) Tab(text: tab),
-          ],
+        body: const Center(
+          child: Text('Weather App'),
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-// hint for the to do task:Considering creating the different for different tabs
-          for (final tab in tabs)
-            Center(
-              child: Text(tab),
-            ),
-        ],
       ),
     );
   }
